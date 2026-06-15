ac0bc47d1232b78dd4ebe38fb62421769ae09604
Cauan brusco <cauan_brusco@estudante.sesisenai.org.br>
2026-06-15 20:07:25 -0300

    q11

--- Program.cs
+++ Program.cs
@@ -1,23 +1,7 @@
-﻿Console.Write("Informe a idade do passageiro: ");
-int idade = int.Parse(Console.ReadLine());
+﻿Console.Write("Informe o número final da contagem: ");
+int numero = int.Parse(Console.ReadLine());
 
-Console.Write("Informe o valor normal da passagem: ");
-double valor = double.Parse(Console.ReadLine());
-
-double pagar;
-
-if (idade <= 5)
+for (int i = 1; i <= numero; i++)
 {
-    pagar = 0;
+    Console.WriteLine(i);
 }
-else if (idade >= 60)
-{
-    pagar = valor / 2;
-}
-else
-{
-    pagar = valor;
-}
-
-Console.WriteLine($"Valor normal da passagem: R$ {valor:F2}");
-Console.WriteLine($"Valor a pagar: R$ {pagar:F2}");
